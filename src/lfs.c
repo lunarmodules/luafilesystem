8db97a44e4e2a4c51daed576e0e0d4bb7ca704c2
Eric Cosatto <cosatto@nec-labs.com>
2016-05-24 16:43:37 -0400

    Update lfs.c

--- src/lfs.c
+++ src/lfs.c
@@ -887,7 +887,7 @@ static const struct luaL_Reg fslib[] = {
         {NULL, NULL},
 };
 
-lfsexp int luaopen_lfs (lua_State *L) {
+lfs_export int luaopen_lfs (lua_State *L) {
         dir_create_meta (L);
         lock_create_meta (L);
         luaL_newlib (L, fslib);
